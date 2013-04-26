544de22c4864429f5e8c409dbf820f7ead019139
inchoki <inchoki@inchoki.com>
2013-04-27 03:28:50 +0900

    Change

--- init.el.d/00_general.el
+++ init.el.d/00_general.el
@@ -1,6 +1,10 @@
 ;; For Showing Line Number
 (global-linum-mode t)
 
+;; For Showing Line/Column Number in Mode line
+(column-number-mode t)
+(line-number-mode t)
+
 ;; For Drag and Drop Region Copy
 (setq mouse-drag-copy-region t)
 
--- init.el.d/20_timestamp.el
+++ init.el.d/20_timestamp.el
@@ -1,5 +1,5 @@
 ;; Auto Insert Time Stamp
-;; If there is 'Time-stamp: <Fri  26 26 23:49:16 JST 2013>' or 'Time-stamp: " "' in the file
+;; If there is 'Time-stamp: <>' or 'Time-stamp: ""' in the file
 ;; less than 8 lines or equal from head, A time-stamp is inserted
 ;; automaticaly. 
 
@@ -13,4 +13,4 @@
 (if (not(memq 'time-stamp-with-locale-c write-file-functions))
     (add-hook 'write-file-functions 'time-stamp-with-locale-c))
 
-(setq time-stamp-format "%3a %3d %02d %02H:%02M:%02S %Z %:y")
+(setq time-stamp-format "%3b/%02d/%:y %3a %02H:%02M:%02S %Z")
